96f3cd82a4a8fe86df4f94103c3b45e1546b8217
sbaha88 <sbaha88@gmail.com>
2019-03-03 18:21:26 +0600

    updating

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -4,40 +4,68 @@ module OmniAuth
   module Strategies
     # Authentication strategy for connecting with the Office365 API.
     class Office365 < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
-      option :name, 'office365'
 
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OAuth gem.
-      option :client_options, :site => 'https://outlook.office.com/',
-                              :authorize_url => 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-                              :token_url => 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+      option :name, :microsoft_graph_auth
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
-      uid { raw_info['Id'] }
+      option :client_options, {
+        site:          'https://login.microsoftonline.com',
+        token_url:     '/common/oauth2/v2.0/token',
+        authorize_url: '/common/oauth2/v2.0/authorize'
+      }
 
-      # https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema#schema-10-and-later
+      option :authorize_options, %i[display score auth_type scope prompt login_hint domain_hint response_mode]
+
+      uid { raw_info["id"] }
+
       info do
         {
-          :name => raw_info['DisplayName'],
-          :email => raw_info['EmailAddress'],
-          :nickname => raw_info['Alias']
+          email:      raw_info["mail"] || raw_info["userPrincipalName"],
+          first_name: raw_info["givenName"],
+          last_name:  raw_info["surname"],
+          name:       full_name,
+          nickname:   raw_info["userPrincipalName"],
         }
       end
 
       extra do
         {
-          :raw_info => raw_info
+          'raw_info' => raw_info,
+          'params' => access_token.params
         }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get('api/v2.0/me').parsed
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
+
+      def raw_info
+        @raw_info ||= access_token.get('https://graph.microsoft.com/v1.0/me').parsed
+      end
+
+      def authorize_params
+        super.tap do |params|
+          %w[display score auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+        end
+      end
+
+      def full_name
+        raw_info["displayName"].presence || raw_info.values_at("givenName", "surname").compact.join(' ')
+      end
+
+      def build_access_token
+        if request.params['access_token']
+          ::OAuth2::AccessToken.from_hash(client, request.params.dup)
+        else
+          super
+        end
+      end
+
+
+
     end
   end
 end
